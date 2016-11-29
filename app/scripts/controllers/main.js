d1633f29aa33fa7508f39ee57113dab67631a38e
PereMekna <dupont.louis4@gmail.com>
2016-11-29 21:22:47 +0100

    Some design change, real time for indicator

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,19 +8,35 @@
  * Controller of the pauseApp
  */
 angular.module('pauseApp')
-  .controller('MainCtrl', function ($scope, $filter) {
+  .controller('MainCtrl', function ($scope, $timeout) {
   	$scope.date = new Date();
   	$scope.pause = "Non !";
+  	$scope.tickInterval = 1000; //ms
 
-  	$scope.init = function () {
-  		if ($scope.date.getHours() == 15 && $scope.date.getMinutes() > 30 && $scope.date.getMinutes() < 45) {
+  	var tick = function() {
+  	    $scope.date = new Date() // get the current time
+  	    $scope.checkPause();
+  	    $timeout(tick, $scope.tickInterval); // reset the timer
+  	}
+
+  	// Start the timer
+  	$timeout(tick, $scope.tickInterval);
+
+  	$scope.checkPause = function () {
+  		if ($scope.date.getHours() == 15 && $scope.date.getMinutes() >= 30 && $scope.date.getMinutes() <= 45) {
   			$scope.pause = "Oui !";
   		}
-  		if ($scope.date.getHours() == 10 && $scope.date.getMinutes() > 15 && $scope.date.getMinutes() < 30) {
+  		if ($scope.date.getHours() == 10 && $scope.date.getMinutes() >= 15 && $scope.date.getMinutes() <= 30) {
+  			$scope.pause = "Oui !";
+  		}
+  		if ($scope.date.getHours() == 12 && $scope.date.getMinutes() >= 15) {
+  			$scope.pause = "Oui !";
+  		}
+  		if ($scope.date.getHours() == 13 && $scope.date.getMinutes() <= 40) {
   			$scope.pause = "Oui !";
   		}
   	}
 
-  	$scope.init();
+  	$scope.checkPause();
 
   });
