5ac89b0eb6f3034524d058f350ca2a6a51e9e91b
PereMekna <dupont.louis4@gmail.com>
2016-11-29 22:40:00 +0100

    add some design change

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,30 +10,41 @@
 angular.module('pauseApp')
   .controller('MainCtrl', function ($scope, $timeout) {
   	$scope.date = new Date();
-  	$scope.pause = "Non !";
+  	$scope.message = "Non !";
   	$scope.tickInterval = 1000; //ms
+  	$scope.pauses = [{'beginHour': 10, 'stopHour': 10, 'beginMinute': 15, 'stopMinute': 30}, {'beginHour': 12, 'stopHour': 13, 'beginMinute': 15, 'stopMinute': 40}, {'beginHour': 15, 'stopHour': 15, 'beginMinute': 30, 'stopMinute': 45}];
+
 
   	var tick = function() {
   	    $scope.date = new Date() // get the current time
   	    $scope.checkPause();
   	    $timeout(tick, $scope.tickInterval); // reset the timer
+
   	}
 
   	// Start the timer
   	$timeout(tick, $scope.tickInterval);
 
   	$scope.checkPause = function () {
-  		if ($scope.date.getHours() == 15 && $scope.date.getMinutes() >= 30 && $scope.date.getMinutes() <= 45) {
-  			$scope.pause = "Oui !";
+  		let isPause = false;
+  		let date = $scope.date;
+  		for (var i = 0; i < $scope.pauses.length; i++) {
+  			let pause = $scope.pauses[i];
+  			if (pause.beginHour != pause.stopHour) {
+  				if ((pause.beginHour === date.getHours() && pause.beginMinute <= date.getMinutes()) || (pause.stopHour === date.getHours() && pause.stopMinute > date.getMinutes())) {
+  					isPause = true;
+  				}
+  			}
+  			if (pause.beginHour === pause.stopHour) {
+  				if ((pause.beginHour === date.getHours() && pause.beginMinute <= date.getMinutes() && pause.stopMinute > date.getMinutes())) {
+  					isPause = true;
+  				}
+  			}
   		}
-  		if ($scope.date.getHours() == 10 && $scope.date.getMinutes() >= 15 && $scope.date.getMinutes() <= 30) {
-  			$scope.pause = "Oui !";
-  		}
-  		if ($scope.date.getHours() == 12 && $scope.date.getMinutes() >= 15) {
-  			$scope.pause = "Oui !";
-  		}
-  		if ($scope.date.getHours() == 13 && $scope.date.getMinutes() <= 40) {
-  			$scope.pause = "Oui !";
+  		if (isPause) {
+  			$scope.message = "Oui !";
+  		} else {
+  			$scope.message = "Non !";
   		}
   	}
 
